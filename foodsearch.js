a8381d963e94399fe997471f1ed99104841faaf5
chriscrichter <chris_richter@outlook.com>
2020-01-21 20:14:29 -0500

    Chris food search code in foodsearch.js.

--- foodsearch.js
+++ foodsearch.js
@@ -16,26 +16,59 @@
     }).then(function(response){
 
             var foodDiv = $("<div class='foodItem'>");
-            var foodName = $('<h2>').addClass('nameFood').text('Food Name: ' + foodField);
+            
+            $("#wrapper").append(foodDiv);
 
+            
+            var foodSup = $('<h2>').text('Nutrition Facts')
+      		foodDiv.append(foodSup);
+            
+            var foodName = $('<h3>').text('Food Name: ' + foodField.toUpperCase());
+			foodDiv.append(foodName);
+			
             var valCalorie = response.calories;
             var foodCalorie = $("<p>").text("Total Calories: " + Math.round(valCalorie));
             foodDiv.append(foodCalorie);
             
-            var valWeight = response.totalWeight;
-            var foodWeight = $("<p>").text("Total Weight: " + Math.round(valWeight));
-            foodDiv.append(foodWeight);
+            if (response.totalNutrients.FAT) {
+            var valFat = response.totalNutrients.FAT.quantity;
+            var valFatUnit = response.totalNutrients.FAT.unit;
+            var foodFat = $("<p>").text("Total Fat: " + Math.round(valFat) + valFatUnit);
+            foodDiv.append(foodFat);
+            }
             
+            if (response.totalNutrients.CHOLE) {
             var valCholesterol = response.totalNutrients.CHOLE.quantity;
-            var foodCholesterol = $("<p>").text("Total Cholesterol: " + Math.round(valCholesterol));
+            var valCholesterolUnit = response.totalNutrients.CHOLE.unit;
+            var foodCholesterol = $("<p>").text("Total Cholesterol: " + Math.round(valCholesterol) + valCholesterolUnit);
             foodDiv.append(foodCholesterol);
+            }
             
-            $("#wrapper").append(foodName);
-            $("#wrapper").append(foodDiv);
+            if (response.totalNutrients.NA) {
+            var valSodium = response.totalNutrients.NA.quantity;
+            var valSodiumUnit = response.totalNutrients.NA.unit;
+            var foodSodium = $("<p>").text("Total Sodium: " + Math.round(valSodium)  + valSodiumUnit);
+            foodDiv.append(foodSodium);
+            }
             
+            if (response.totalNutrients.CHOCDF) {
+            var valCarbohydrate = response.totalNutrients.CHOCDF.quantity;
+            var valCarbohydrateUnit = response.totalNutrients.CHOCDF.unit;
+            var foodCarbohydrate = $("<p>").text("Total Carbohydrate: " + Math.round(valCarbohydrate)  + valCarbohydrateUnit);
+            foodDiv.append(foodCarbohydrate);
+            }
+            
+            if (response.totalNutrients.PROCNT) {
+            var valProtein = response.totalNutrients.PROCNT.quantity;
+            var valProteinUnit = response.totalNutrients.PROCNT.unit;
+            var foodProtein = $("<p>").text("Total Protein: " + Math.round(valProtein) + valProteinUnit);
+            foodDiv.append(foodProtein);
+            }
+                           
     });
   };
   
+/*
    // Spoonacular api request for food fact
   $.ajax({
     url: "https://api.spoonacular.com/food/trivia/random?apiKey=6955b593214a4bbebdb0ce41be4872ad",
@@ -51,7 +84,7 @@
     factDiv.append(pOne);
 
 });
-
+*/
 
 
 
@@ -65,17 +98,17 @@
 
     btn.onclick = function() {
       modal.style.display = "block";
-}
+};
 
     span.onclick = function() {
       modal.style.display = "none";
-}
+};
 
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
-  }
-}
+  };
+};
 
    // Home button
   $('#home').on('click',function() {
